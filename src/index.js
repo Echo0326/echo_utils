566a1f713d3208c81a555db5d7842b166e6adfb1
chuwei <chuwei@ambrightgroup.com>
2021-06-30 09:32:48 +0800

    1

--- src/index.js
+++ src/index.js
@@ -1,212 +1,212 @@
 /**
-* @author chuwei
-* @date 2021/3/10 10:11
-* @description:校验手机号
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:11
+ * @description:校验手机号
+ **/
 function checkPhone(phone) {
-  return /^1\d{10}$/.test(phone)
+	return /^1\d{10}$/.test(phone)
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:12
-* @description:校验邮箱
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:12
+ * @description:校验邮箱
+ **/
 function checkMail(mail) {
-  return /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(mail)
+	return /\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(mail)
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:12
-* @description:校验url
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:12
+ * @description:校验url
+ **/
 function checkUrl(url) {
-  return /^https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\?)?=?&?[a-zA-Z0-9_-](\?)?)*)*$/i.test(url)
+	return /^https?:\/\/(([a-zA-Z0-9_-])+(\.)?)*(:\d+)?(\/((\.)?(\?)?=?&?[a-zA-Z0-9_-](\?)?)*)*$/i.test(url)
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:12
-* @description:校验身份证
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:12
+ * @description:校验身份证
+ **/
 function checkIdCard(card) {
-  return /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/.test(card)
+	return /^(^[1-9]\d{7}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])\d{3}$)|(^[1-9]\d{5}[1-9]\d{3}((0\d)|(1[0-2]))(([0|1|2]\d)|3[0-1])((\d{4})|\d{3}[Xx])$)$/.test(card)
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:12
-* @description:校验邮编
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:12
+ * @description:校验邮编
+ **/
 function checkPostCode(postcode) {
-  return /^[1-9]\d{5}(?!\d)$/.test(postcode)
+	return /^[1-9]\d{5}(?!\d)$/.test(postcode)
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:12
-* @description:校验日期
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:12
+ * @description:校验日期
+ **/
 function checkDate(date) {
-  return /^[1-2][0-9][0-9][0-9]-[0-1]{0,1}[0-9]-[0-3]{0,1}[0-9]$/.test(date)
+	return /^[1-2][0-9][0-9][0-9]-[0-1]{0,1}[0-9]-[0-3]{0,1}[0-9]$/.test(date)
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:12
-* @description:校验ios
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:12
+ * @description:校验ios
+ **/
 function checkIos() {
-  return /iphone/i.test(navigator.userAgent)
+	return /iphone/i.test(navigator.userAgent)
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:13
-* @description:校验安卓
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:13
+ * @description:校验安卓
+ **/
 function checkAndroid() {
-  return /android/i.test(navigator.userAgent)
+	return /android/i.test(navigator.userAgent)
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:13
-* @description:校验移动端
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:13
+ * @description:校验移动端
+ **/
 function checkMobile() {
-  return /(nokia|iphone|android|ipad|motorola|^mot\-|softbank|foma|docomo|kddi|up\.browser|up\.link|htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|blackberry|alcatel|amoi|ktouch|nexian|samsung|^sam\-|s[cg]h|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte\-|longcos|pantech|gionee|^sie\-|portalmmm|jig\s browser|hiptop|^ucweb|^benq|haier|^lct|opera\s*mobi|opera\*mini|320x320|240x320|176x220)/i.test(navigator.userAgent)
+	return /(nokia|iphone|android|ipad|motorola|^mot\-|softbank|foma|docomo|kddi|up\.browser|up\.link|htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|blackberry|alcatel|amoi|ktouch|nexian|samsung|^sam\-|s[cg]h|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte\-|longcos|pantech|gionee|^sie\-|portalmmm|jig\s browser|hiptop|^ucweb|^benq|haier|^lct|opera\s*mobi|opera\*mini|320x320|240x320|176x220)/i.test(navigator.userAgent)
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:13
-* @description:校验微信
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:13
+ * @description:校验微信
+ **/
 function checkWeChat() {
-  return /MicroMessenger/i.test(navigator.userAgent)
+	return /MicroMessenger/i.test(navigator.userAgent)
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:13
-* @description:浅复制
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:13
+ * @description:浅复制
+ **/
 function copy(target) {
-  let res
-  let targetType = checkType(target)
-  if (targetType === 'Object') {
-    res = {}
-  } else if (targetType === 'Array') {
-    res = []
-  } else {
-    return target
-  }
-  for (let i in target) {
-    let value = target[i]
-    if (checkType(value) === 'Object' || checkType(value) === 'Array') {
-      res[i] = copy(value)
-    } else {
-      res[i] = value
-    }
-  }
-  return res
+	let res
+	let targetType = checkType(target)
+	if (targetType === 'Object') {
+		res = {}
+	} else if (targetType === 'Array') {
+		res = []
+	} else {
+		return target
+	}
+	for (let i in target) {
+		let value = target[i]
+		if (checkType(value) === 'Object' || checkType(value) === 'Array') {
+			res[i] = copy(value)
+		} else {
+			res[i] = value
+		}
+	}
+	return res
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:13
-* @description:设置一个Session
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:13
+ * @description:设置一个Session
+ **/
 function setSessionStorage(key, value) {
-  if (window.sessionStorage) {
-    sessionStorage.setItem(key, JSON.stringify(value))
-  } else {
-    alert('您的浏览器不支持sessionStorage存储')
-  }
+	if (window.sessionStorage) {
+		sessionStorage.setItem(key, JSON.stringify(value))
+	} else {
+		alert('Your browser does not support session storage')
+	}
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:14
-* @description:获取一个Session
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:14
+ * @description:获取一个Session
+ **/
 function getSessionStorage(key) {
-  if (window.sessionStorage) {
-    return JSON.parse(sessionStorage.getItem(key))
-  } else {
-    alert('您的浏览器不支持sessionStorage存储')
-  }
+	if (window.sessionStorage) {
+		return JSON.parse(sessionStorage.getItem(key))
+	} else {
+		alert('Your browser does not support session storage')
+	}
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:14
-* @description:删除一个Session
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:14
+ * @description:删除一个Session
+ **/
 function removeSessionStorage(key) {
-  if (window.sessionStorage) {
-    sessionStorage.removeItem(key)
-  } else {
-    alert('您的浏览器不支持sessionStorage存储')
-  }
+	if (window.sessionStorage) {
+		sessionStorage.removeItem(key)
+	} else {
+		alert('Your browser does not support session storage')
+	}
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:14
-* @description:设置一个LocalStorage
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:14
+ * @description:设置一个LocalStorage
+ **/
 function setLocalStorage(key, value) {
-  if (window.localStorage) {
-    localStorage.setItem(key, JSON.stringify(value))
-  } else {
-    alert('您的浏览器不支持localStorage存储')
-  }
+	if (window.localStorage) {
+		localStorage.setItem(key, JSON.stringify(value))
+	} else {
+		alert('Your browser does not support localstorage storage')
+	}
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:14
-* @description:获取一个LocalStorage
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:14
+ * @description:获取一个LocalStorage
+ **/
 function getLocalStorage(key) {
-  if (window.localStorage) {
-    return JSON.parse(localStorage.getItem(key))
-  } else {
-    alert('您的浏览器不支持localStorage存储')
-  }
+	if (window.localStorage) {
+		return JSON.parse(localStorage.getItem(key))
+	} else {
+		alert('Your browser does not support localstorage storage')
+	}
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:14
-* @description:删除一个LocalStorage
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:14
+ * @description:删除一个LocalStorage
+ **/
 function removeLocalStorage(key) {
-  if (window.localStorage) {
-    localStorage.removeItem(key)
-  } else {
-    alert('您的浏览器不支持localStorage存储')
-  }
+	if (window.localStorage) {
+		localStorage.removeItem(key)
+	} else {
+		alert('Your browser does not support localstorage storage')
+	}
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 9:59
-* @description:获取url上的query参数
-**/
+ * @author chuwei
+ * @date 2021/3/10 9:59
+ * @description:获取url上的query参数
+ **/
 function getQuery(key) {
 	return decodeURI(window.location.search.replace(
 		new RegExp("^(?:.*[&\\?]" + encodeURI(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"))
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:01
-* @description:生成n-m的随机数
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:01
+ * @description:生成n-m的随机数
+ **/
 function random(n, m) {
 	n = +n || 0
 	m = +m || 0
@@ -214,10 +214,10 @@ function random(n, m) {
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:05
-* @description:生成n-m的随机整数
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:05
+ * @description:生成n-m的随机整数
+ **/
 function randomInt(n, m) {
 	n = +n || 0
 	m = +m || 0
@@ -225,15 +225,51 @@ function randomInt(n, m) {
 }
 
 /**
-* @author chuwei
-* @date 2021/3/10 10:20
-* @description:获取一个对象的数据类型
-**/
+ * @author chuwei
+ * @date 2021/3/10 10:20
+ * @description:获取一个对象的数据类型
+ **/
 function getObjType(obj) {
-	return Object.prototype.toString.call(obj).slice(8,-1)
+	return Object.prototype.toString.call(obj).slice(8, -1)
 }
 
-const echo_utils={
+/**
+ * @author chuwei
+ * @date 2021/6/23 11:29
+ * @description:防抖函数
+ **/
+function debounce(fn, wait, immediate) {
+	let timer = null
+	return function (...args) {
+		if (immediate && !timer) {
+			fn.apply(this, args)
+		}
+		timer && clearTimeout(timer)
+		timer = setTimeout(() => {
+			fn.apply(this, args)
+		}, wait)
+	}
+}
+
+
+/**
+ * @author chuwei
+ * @date 2021/6/23 11:30
+ * @description:节流函数
+ **/
+function throttle(fn, wait) {
+	let timer = null
+	return function (...args) {
+		if (!timer) {
+			timer = setTimeout(() => {
+				fn.apply(this, args)
+				timer = null
+			}, wait)
+		}
+	}
+}
+
+const echo_utils = {
 	checkPhone,
 	checkMail,
 	checkUrl,
@@ -254,11 +290,14 @@ const echo_utils={
 	getQuery,
 	random,
 	randomInt,
-	getObjType
+	getObjType,
+	debounce,
+	throttle
 }
 
-if (typeof window !=="undefined"){
-	window.echo_utils=echo_utils
+if (typeof window !== "undefined") {
+	window.echo_utils = echo_utils
 }
+
 
 module.exports = echo_utils
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,24 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'index.js',
+		library: 'echo-utils', // 指定的就是你使用require时的模块名
+		libraryTarget: 'umd', // 指定输出格式
+		umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define
 	},
-	mode:'production'
+	mode:'production',
+	module: {
+		rules: [
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+						plugins: ['@babel/plugin-transform-runtime']
+					}
+				}
+			}
+		]
+	}
 };
